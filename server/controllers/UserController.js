ae68aa179ba6636dbc69f4914a7b3bbdd10e8280
Eli Davis <eli.davis42@gmail.com>
2022-02-23 11:12:19 -0500

    createUser initialized

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,12 +1,28 @@
 const User = require('../models/UserModel.js');
-const axios = require('axios');
 
 require('dotenv').config();
 
 const userController = {};
 
 userController.createUser = async (req, res, next) => {
-
+    const { firstName, username, password, preferDarkMode } = req.body;
+    try {
+        const newUser = await User.create({
+            firstName: firstName,
+            username: username,
+            password: password,
+            preferDarkMode: preferDarkMode,
+        })
+        res.locals.newUser = newUser;
+        return next();
+    }
+    catch (err) {
+        console.log(err)
+        return next({
+            log: `userController.createUser: ERROR: ${typeof err === 'object' ? JSON.stringify(err) : err}`,
+            message: { err: 'Error occurred in userController.createUser. Check server logs for more details.' },
+          })
+    }
 }
 
 userController.getUser = async (req, res, next) => {
@@ -20,3 +36,5 @@ userController.updateUser = async (req, res, next) => {
 userController.deleteUser = async (req, res, next) => {
     
 }
+
+module.exports = userController;
