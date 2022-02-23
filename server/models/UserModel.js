73549c7c7a0882e9d9de84d7ecc5ffb7174a1547
Eli Davis <eli.davis42@gmail.com>
2022-02-23 12:03:33 -0500

    implemented password hash

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
     firstName: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
+    username: {type: String, required: true},
     password: {type: String, required: true},
     preferDarkMode: {type: Boolean},
   });
